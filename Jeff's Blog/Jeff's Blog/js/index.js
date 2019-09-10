6cac7ce7363f60c1b11f324043d59c356bb164fe
Emanuel Augusto <emanuel02augusto@gmail.com>
2019-09-10 09:27:22 -0300

    Competencias de html5, css3 e javascript

---
+++ Jeff's Blog/Jeff's Blog/js/index.js
@@ -0,0 +1,11 @@
+$(".zero").on("click", function() {
+      alert("Você deu  0 estrelas para Jeff");
+});
+
+$(".one").on("click", function() {
+      alert("Você deu  0.5 estrela para Jeff");
+});
+
+$(".two").on("click", function() {
+      alert("Você deu  1 estrela para Jeff");
+});
